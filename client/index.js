1372f98cc32a2e5326e5461ca1785e76521872cc
niksrj19 <58848186+niksrj19@users.noreply.github.com>
2022-03-29 10:15:16 +0530

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,11 +1,42 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache
+} from '@apollo/client';
+import "./style/style.css"
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  IndexRoute,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import SongLists from './components/SongLists';
+import App from "./components/App";
+import SongCreate from './components/SongCreate';
+import SongDetails from './components/SongDetails';
+const client =new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache:new InMemoryCache(),
+  dataFromObject :o=>o.id  // update when child record hange 
+});
 const Root = () => {
-  return <div>Lyrical</div>
+  return (
+    <Router >
+      <Routes>
+        <Route exact path="/" element={<SongLists/>}/>
+        <Route exact path="/createsong" element={<SongCreate/>}/>
+        <Route exact path="/song/:id" element={<SongDetails/>} />
+       </Routes>
+      
+    </Router>
+    )
 };
-
 ReactDOM.render(
-  <Root />,
+  <ApolloProvider client={client}><Root /></ApolloProvider>,
   document.querySelector('#root')
 );
